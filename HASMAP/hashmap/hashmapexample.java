8f870c1ca6bcf8359907c8f0630cb323cfd6b4e1
vaishali312003 <va3har@gmail.com>
2024-03-08 17:19:43 +0530

    ADDED HASH FILE

---
+++ HASMAP/hashmap/hashmapexample.java
@@ -0,0 +1,10 @@
+import java.util.HashMap;
+
+public class hashmapexample{
+    public static void main(String[] args) {
+        HashMap<String,Integer> var=new HashMap<>();
+        var.put("INDIA",10);
+        System.out.println(var);
+    }
+}
+
